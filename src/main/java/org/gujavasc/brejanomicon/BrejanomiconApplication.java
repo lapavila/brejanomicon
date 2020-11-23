ac9efb3a22324cdcd9097d6b35d3d343e77c91dd
Luiz Avila <lapavila@gmail.com>
2020-11-23 09:56:40 -0300

    Add swagger docs

--- src/main/java/org/gujavasc/brejanomicon/BrejanomiconApplication.java
+++ src/main/java/org/gujavasc/brejanomicon/BrejanomiconApplication.java
@@ -1,7 +1,12 @@
 package org.gujavasc.brejanomicon;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Arrays;
 
 @SpringBootApplication
 public class BrejanomiconApplication {
@@ -9,5 +14,4 @@ public class BrejanomiconApplication {
     public static void main(String[] args) {
         SpringApplication.run(BrejanomiconApplication.class, args);
     }
-
 }
